731e59fcae24468ecb59b7c81f1c65a5ea23b985
mininpark <parkmini.93@gmail.com>
2022-05-17 21:24:52 +0200

    inital settng and navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      gold: '#DCCA87',
+      black: '#0C0C0C',
+      gray: '#545454',
+      crimson: '#F5EFDB',
+      grey: '#AAAAAA',
+      white:'#FFFFFF',
+    },
+  },
+  plugins: [],
+}
